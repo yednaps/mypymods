622a1e1914340602f2d6a3a7053a81e6fd68790b
sp <yednap@gmail.com>
2015-08-13 11:30:12 -0700

    refactored dtdelta to unpack arguments and add default value of NOW for second arg

--- datedelta/dtdelta.py
+++ datedelta/dtdelta.py
@@ -2,18 +2,13 @@
 
 import datetime, sys
 
-def days2(dat1,dat2):
+def days2(dat1,dat2=datetime.datetime.now().strftime('%m%d%y')):
     dat1 = datetime.datetime.strptime(dat1,'%m%d%y')
-    if dat2:
-        dat2 = datetime.datetime.strptime(dat2,'%m%d%y')
-    else:
-        dat2 = datetime.datetime.now()
+    dat2 = datetime.datetime.strptime(dat2,'%m%d%y')
     return (dat1-dat2).days
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Usage: dtdelta DATE2 [Optional DATE1 - now if not provided]")
-    elif len(sys.argv) == 2:
-        print(days2(sys.argv[1],None))
     else:
-        print(days2(sys.argv[1],sys.argv[2]))
+        print(days2(*sys.argv[1:]))
