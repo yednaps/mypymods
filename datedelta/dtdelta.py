d33587ac23e76863779c0b6834dde64701abe9a7
sp <yednap@gmail.com>
2015-08-13 11:18:01 -0700

    modified dtdelta to accept two dates as arguments

--- datedelta/dtdelta.py
+++ datedelta/dtdelta.py
@@ -2,10 +2,18 @@
 
 import datetime, sys
 
-def days2(dat):
-    dat = datetime.datetime.strptime(dat,'%m%d%y')
-    now = datetime.datetime.now()
-    return (dat-now).days
+def days2(dat1,dat2):
+    dat1 = datetime.datetime.strptime(dat1,'%m%d%y')
+    if dat2:
+        dat2 = datetime.datetime.strptime(dat2,'%m%d%y')
+    else:
+        dat2 = datetime.datetime.now()
+    return (dat1-dat2).days
 
 if __name__ == "__main__":
-    print days2(sys.argv[1])
+    if len(sys.argv) == 1:
+        print("Usage: dtdelta DATE2 [Optional DATE1 - now if not provided]")
+    elif len(sys.argv) == 2:
+        print(days2(sys.argv[1],None))
+    else:
+        print(days2(sys.argv[1],sys.argv[2]))
